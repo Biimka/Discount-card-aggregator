8855461349af41431cc62c73024282d8b88ef499
Artyom Sklyanichenko <sklyart@Artyoms-MacBook-Air.local>
2018-11-16 09:53:41 +0400

    DCA-4. CardList

---
+++ app/src/main/java/com/startandroid/biimka/discount_card_aggregator/card_list/CardListPresenter.java
@@ -0,0 +1,8 @@
+package com.startandroid.biimka.discount_card_aggregator.card_list;
+
+import com.arellomobile.mvp.InjectViewState;
+import com.arellomobile.mvp.MvpPresenter;
+
+@InjectViewState
+public class CardListPresenter extends MvpPresenter<CardListView> {
+}
