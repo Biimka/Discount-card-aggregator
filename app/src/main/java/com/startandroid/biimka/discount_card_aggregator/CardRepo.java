6643ff99e214351384826074ad6d1bc056b870f8
Artyom Sklyanichenko <sklyart@Artyoms-MacBook-Air.local>
2018-11-16 10:16:02 +0400

    DCA-5. CardRepo

---
+++ app/src/main/java/com/startandroid/biimka/discount_card_aggregator/CardRepo.java
@@ -0,0 +1,11 @@
+package com.startandroid.biimka.discount_card_aggregator;
+
+import java.util.List;
+
+public interface CardRepo {
+    public List<Card> getCards();
+    public Card getCard(long id);
+    public void createCard(Card card);
+    public void updateCard(Card card);
+    public void deleteCard(long id);
+}
