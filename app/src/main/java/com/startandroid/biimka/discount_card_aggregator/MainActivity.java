649cc21313c29cf209881bfd24025ec73b41eb38
Artyom Sklyanichenko <sklyart@Artyoms-MacBook-Air.local>
2018-12-11 19:54:33 +0400

    DCA-3. Not modified MainActivity

--- app/src/main/java/com/startandroid/biimka/discount_card_aggregator/MainActivity.java
+++ app/src/main/java/com/startandroid/biimka/discount_card_aggregator/MainActivity.java
@@ -1,7 +1,7 @@
 package com.startandroid.biimka.discount_card_aggregator;
 
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
