bac1b1bfad576991f86bc9c575bd8546174279af
Biimka <olya_samolova@mail.ru>
2018-12-19 15:18:09 +0400

    Revert "DCA-5. Rebase"
    This reverts commit c8c50b2b3c68b0da62c9acc46412fedf86a9d8c1.

---
+++ app/src/main/java/com/startandroid/biimka/discount_card_aggregator/CardListFragment.java
@@ -0,0 +1,37 @@
+package com.startandroid.biimka.discount_card_aggregator;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
+public class CardListFragment extends Fragment {
+
+    public static CardListFragment newInstance() {
+        return new CardListFragment();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        final View rootView = inflater.inflate(R.layout.fragment_cardlist, null);
+
+        final NavController navController = Navigation.findNavController(getActivity(), R.id.nav_host_fragment);
+
+        (rootView.findViewById(R.id.addDiscountCardFAB)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                navController.navigate(R.id.fragmentCard);
+            }
+        });
+
+        return rootView;
+    }
+
+}
