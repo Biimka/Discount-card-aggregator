baf1f1eee0f243fbc7642f91f4ce0cb0aa3ea977
Biimka <olya_samolova@mail.ru>
2018-12-19 20:47:57 +0400

    DCA-4. Pull&squash

--- app/src/main/java/com/startandroid/biimka/discount_card_aggregator/CardRepo.java
+++ app/src/main/java/com/startandroid/biimka/discount_card_aggregator/CardRepo.java
@@ -4,12 +4,8 @@ import java.util.List;
 
 public interface CardRepo {
     public List<Card> getCards();
-
     public Card getCard(long id);
-
-    public void createCard(Card card);
-
+    public void createCard(String name, byte[] frontImage, byte[] backImage, long barcode);
     public void updateCard(Card card);
-
     public void deleteCard(long id);
 }
